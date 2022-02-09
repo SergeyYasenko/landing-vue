a9561f969c45425db1faf79a43bab9686cc0dfc4
NikSoftgrad <nik_k@softgrad.com>
2022-02-09 13:16:29 +0300

    bugfix "header-page"

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,54 @@
 <template>
-   <div id="app">
-      <Header />
-   </div>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
-import Header from './components/header.vue'
+import Header from "./components/header.vue";
 
 export default {
-   name: 'App',
-   components: {
-      Header,
-   }
-}
-
+  name: "App",
+  components: {
+    Header,
+  },
+};
 </script>
 
 <style>
-
-@import url('https://fonts.googleapis.com/css2?family=BioRhyme:wght@700&family=DM+Sans:wght@400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=BioRhyme:wght@700&family=DM+Sans:wght@400;700&display=swap");
 
 *,
 *::before,
 *::after {
-   box-sizing: border-box;
-   padding: 0;
-   margin: 0;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
 }
 
 ul,
 ol {
-   list-style: none;
+  list-style: none;
 }
 
 body {
-   min-height: 100vh;
-   scroll-behavior: smooth;
-   text-rendering: optimizeSpeed;
-   margin: 0;
-   line-height: 1.5;
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  text-rendering: optimizeSpeed;
+  margin: 0;
+  line-height: 1.5;
 }
 
 #app {
-   font-family: 'BioRhyme',serif, 'DM Sans', sans-serif ;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   color: #333;
+  font-family: "BioRhyme", serif, "DM Sans", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
 }
 
 .container {
-   margin: 0 auto;
-   padding: 0 15px;
-   max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 15px;
+  max-width: 1200px;
 }
-
-
-
-
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,152 +1,237 @@
 <template >
-   <div class="header">
-      <div class="header__container container">
-         <div class="header__row">
-            <div class="header__logo">
-               <transition class="header__burger-line" name="fade" mode="out-in">
-                  <i class="material-icons menu" v-if="!show" @click="show = !show" key="menu">menu</i>
-                  <i class="material-icons close" v-else @click="show = !show" key="close">close</i>
-               </transition>
-               <nav class="header__burger-menu">
-                  <transition name="fade">
-                     <ul v-if="show">
-                        <li
-                        v-for="item in items"
-                        :key="item.id"
-                        class="header__menu-item"
-                        > {{ item }} </li>
-                     </ul>
-                  </transition>
-               </nav>
-               <h1 class="header__logo-title" >{{message}}</h1>
-            </div>
-            <ul class="header__contact-list">
-               <li class="header__contact-link"><a href="#"><img src="../assets/image/icons/facebook.svg" alt="facebook"></a></li>
-               <li class="header__contact-link"><a href="#"><img src="../assets/image/icons/twitter.svg" alt="twitter"></a></li>
-               <li class="header__contact-link"><a href="#"><img src="../assets/image/icons/instagram.svg" alt="instagram"></a></li>
+  <div class="header">
+    <div class="header__container container">
+      <div class="header__row">
+        <div class="header__menu">
+          <i
+            class="material-icons menu"
+            @click="toggleClass(0)"
+            v-bind:class="{ option_box: true, open: isOpen(0) }"
+            key="menu"
+            >menu</i
+          >
+          <nav
+            v-bind:class="{ option_box: true, open: isOpen(0) }"
+            class="header__burger-menu"
+          >
+            <i
+              class="material-icons close"
+              @click="toggleClass(0)"
+              v-bind:class="{ option_box: true, open: isOpen(0) }"
+              key="close"
+              >close</i
+            >
+            <ul>
+              <li class="header__menu-item">
+                <a
+                  v-for="item in items"
+                  :key="item.title"
+                  :href="item.path"
+                  target="_blank"
+                >
+                  {{ item.title }}
+                </a>
+              </li>
+              <div class="header__contact-list-m">
+                <li class="header__contact-item-m">
+                  <a href="#" target="_blank"
+                    ><img
+                      src="../assets/image/icons/facebook.svg"
+                      alt="facebook"
+                  /></a>
+                </li>
+                <li class="header__contact-item-m">
+                  <a href="#" target="_blank"
+                    ><img src="../assets/image/icons/twitter.svg" alt="twitter"
+                  /></a>
+                </li>
+                <li class="header__contact-item-m">
+                  <a href="#" target="_blank"
+                    ><img
+                      src="../assets/image/icons/instagram.svg"
+                      alt="instagram"
+                  /></a>
+                </li>
+              </div>
             </ul>
-         </div>
+          </nav>
+          <h1 class="header__logo-title">
+            {{ message }}
+          </h1>
+        </div>
+        <ul class="header__contact-list">
+          <li class="header__contact-item">
+            <a href="#" target="_blank"
+              ><img src="../assets/image/icons/facebook.svg" alt="facebook"
+            /></a>
+          </li>
+          <li class="header__contact-item">
+            <a href="#" target="_blank"
+              ><img src="../assets/image/icons/twitter.svg" alt="twitter"
+            /></a>
+          </li>
+          <li class="header__contact-item">
+            <a href="#" target="_blank"
+              ><img src="../assets/image/icons/instagram.svg" alt="instagram"
+            /></a>
+          </li>
+        </ul>
       </div>
-   </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-   name: 'Header',
-   data: function () {
-      return {
-         message: 'ARCH',
-         items: [
-            'Home',
-            'About',
-            'Blog',
-            'Map',
-            'Contacts',
-         ],
-         show: false,
-      }
-   }
+  name: "Header",
+  data: function () {
+    return {
+      message: "ARCH",
+      items: [
+        { title: "Home", path: "#" },
+        { title: "About", path: "#" },
+        { title: "Contact", path: "#" },
+        { title: "Work With Us", path: "#" },
+        { title: "Logout", path: "#" },
+      ],
+      openData: [{ isOpen: false }, { isOpen: false }, { isOpen: false }],
+      close: false,
+    };
+  },
+  methods: {
+    toggleClass(i) {
+      this.openData[i].isOpen = !this.openData[i].isOpen;
+    },
+    isOpen(i) {
+      return this.openData[i].isOpen;
+    },
+  },
 };
-
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 
 .header {
-   width: 100%;
-   padding: 16px 0;
-   background-color:#F7F2E2;
+  width: 100%;
+  padding: 16px 0;
+  background-color: #f7f2e2;
 }
 
 .header__row {
-   display: flex;
-   justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
-.header__logo {
-   display: flex;
+.header__menu {
+  display: flex;
 }
 
 .material-icons {
-   width: 48px;
-   height: 48px;
-   background-color: #fff;
-   color: #DD514C;
-   border-radius: 50%;
-   margin-right: 16px;
-   display: flex;
-   align-items: center;
-   justify-content: center;
+  width: 48px;
+  height: 48px;
+  background-color: #fff;
+  color: #dd514c;
+  border-radius: 50%;
+  margin-right: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease;
 }
 
 .header__burger-line span:last-child {
-   margin: 0;
+  margin: 0;
 }
 
-.fade-enter-active,
-.fade-leave-active {
-   transition: opacity .3s ease;
+.menu {
+  cursor: pointer;
 }
 
-.fade-enter,
-.fade-leave-to {
-   opacity: 0;
+.menu.open {
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.3s ease;
 }
 
-.menu,
 .close {
-   cursor: pointer;
-   transition: all .3s ease;
+  position: absolute;
+  left: 30px;
+  top: 15px;
+  z-index: 4;
+  cursor: pointer;
+  background-color: #f7f2e2;
 }
 
 .header__burger-menu {
-   background-color: #fff;
-   position: absolute;
-   top: 0;
-   left: 0;
-   min-width: 300px;
+  background-color: #fff;
+  position: absolute;
+  top: 0;
+  left: -100%;
+  min-width: 300px;
+  height: 100vh;
+  z-index: 3;
+  transition: all 0.3s ease;
+  padding-top: 60px;
 }
 
-.header__menu-item {
-   width: 100%;
-   text-align: center;
-   padding: 20px 0;
+.header__burger-menu.open {
+  left: 0;
+  transition: all 0.3s ease;
 }
 
-.header__menu-item:first-child {
-   padding-top: 40px;
+.header__menu-item a {
+  color: #333;
+  display: block;
+  width: 100%;
+  text-align: center;
+  padding: 20px 0;
+  text-decoration: none;
 }
-
 
 .header__logo-title {
-   font-weight: bold;
-   font-size: 40px;
-   line-height: 48px;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 48px;
 }
 
-.header__contact-list {
-   display: flex;
+.header__contact-list,
+.header__contact-list-m {
+  display: flex;
 }
 
-.header__contact-link {
-   width: 48px;
-   height: 48px;
-   background-color: #fff;
-   border-radius: 50%;
-   margin-right: 8px;
-   display: flex;
-   align-items: center;
-   justify-content: center;
+.header__contact-list-m {
+  width: 100%;
+  justify-content: center;
+  align-items: center;
 }
 
-.header__contact-link:last-child {
-   margin: 0;
+.header__contact-item a,
+.header__contact-item-m a {
+  width: 48px;
+  height: 48px;
+  background-color: #fff;
+  border-radius: 50%;
+  margin-right: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
+.header__contact-item-m a {
+  background-color: #f7f2e2;
+  margin-top: 30px;
+}
 
+.header__contact-item:last-child,
+.header__contact-item-m:last-child {
+  margin: 0;
+}
+
+@media screen and (max-width: 576px) {
+  .header__contact-list {
+    display: none;
+  }
+}
 </style>
